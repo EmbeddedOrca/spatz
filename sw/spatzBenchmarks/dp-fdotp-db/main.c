22d4541720cdd5ecfc0caf1e12d45db398890179
tlandeg <tlandeg@student.ethz.ch>
2025-06-26 14:41:27 +0200

    Clean up dotp

--- sw/spatzBenchmarks/dp-fdotp-db/main.c
+++ sw/spatzBenchmarks/dp-fdotp-db/main.c
@@ -23,6 +23,8 @@
 #include DATAHEADER
 #include "kernel/fdotp-db.c"
 
+// #define TIMING 1
+
 #define DBG_LVL_NONE 0
 #define DBG_LVL_ERR 1
 #define DBG_LVL_WARN 2
@@ -36,11 +38,15 @@
     func; \
   }
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
 double *l1_buf;
 double *a;
 double *b;
-double *result;
-double dotp_res = 0.0;
+double *result_ma;
+double *result_simple;
+
+double result[2] = { 0 };
 
 static inline int fp_check(const double a, const double b) {
   const double threshold = 0.00001;
@@ -103,148 +109,177 @@ double dp_dotp_db_ma(
   unsigned int vec_dim,
 
   double *l1_buf,
-  unsigned int l1_buf_len, // In sizeof doubles
-
-  double *result
+  unsigned int l1_buf_len
 ) {
-  const unsigned int num_cores = snrt_cluster_core_num();
   const unsigned int cid = snrt_cluster_core_idx();
+  const unsigned int num_cores = snrt_cluster_core_num();
 
   // HW and algo configuration
-  const unsigned int MEMORY_BANKS = 16;
+  const unsigned int CHUNK_SIZE = 8; // Half of L1 banks
   const unsigned int T_S = sizeof(double);
-  const unsigned int CHUNK_SIZE = MEMORY_BANKS / 2; // DMA blocks half of the memory banks
 
-  // The number of chunks we can load at a time is given by the useable memory size
-  // devided by the number of vectors and a factor of 2 for the double buffering
-  // and the size of a single chunk
-  const unsigned int NUM_CHUNKS = l1_buf_len / 2 / 2 / (CHUNK_SIZE);
-  double *a = l1_buf; // First half for the first vector
-  double *b = l1_buf + (l1_buf_len / 2); // Second half for the second vector
+  if (l1_buf_len % 32 != 0) {
+    printf("L1 buffer length must be a multiple of the number of banks times the number of cores (32).\n");
+    return 0.0;
+  }
 
-  // Reset timer
+  // Number of element per iteration
+  const unsigned MAX_NUM_ELEM_BUF = l1_buf_len / 4;
+  const unsigned int NUM_ELEM_PER_ITER = MIN(MAX_NUM_ELEM_BUF, vec_dim / 2);
+
+  // Pointers to the left and right side of L1 memory
+  double *a0 = l1_buf;
+  double *b0 = l1_buf + 2 * NUM_ELEM_PER_ITER;
+  double *a1 = a0 + CHUNK_SIZE;
+  double *b1 = b0 + CHUNK_SIZE;
+  double *temp = NULL;
+  double res = 0.0;
+  result[cid] = 0.0;
+
+  // Pointer increment
+  const unsigned int NUM_ELEM_PER_CORE = NUM_ELEM_PER_ITER / num_cores;
+  const unsigned int CORE_OFFSET = cid * NUM_ELEM_PER_ITER;
+  const unsigned int DMA2D_REPEAT = NUM_ELEM_PER_ITER / CHUNK_SIZE;
+
+  if (cid == 0) {
+    printf("a0: %p, b0: %p, a1: %p, b1: %p\n", a0, b0, a1, b1);
+    printf("MAX_NUM_ELEM_BUF: %u\n", MAX_NUM_ELEM_BUF);
+    printf("NUM_ELEM_PER_ITER: %u\n", NUM_ELEM_PER_ITER);
+    printf("2D DMA REPEAT: %u\n", DMA2D_REPEAT);
+  }
+
+  // Timing
   unsigned int load_timer = (unsigned int)-1;
   unsigned int calc_timer = (unsigned int)-1;
+  unsigned int calc_timer_avg = 0;
+  unsigned dma_wait_tot = 0;
+  unsigned int performance_timer = 0;
 
+  // Current element to load from DRAM
   unsigned int load_idx = 0;
-
-  unsigned num_iter = vec_dim / (CHUNK_SIZE * NUM_CHUNKS);
-  if (cid == 0) {
-    DEBUG(printf("Expected iterations: %u\n", num_iter), DBG_LVL_DBG);
-  }
 
   // Start the initial DMA transfer
   if (cid == 0) {
+    DEBUG(printf("Load index: %u, vec_dim: %u\n", load_idx, vec_dim), DBG_LVL_DBG);
     snrt_dma_start_2d(
-      a,
+      a0,
       dotp_A_dram + load_idx,
       CHUNK_SIZE * T_S,
       2 * CHUNK_SIZE * T_S,
       CHUNK_SIZE * T_S,
-      NUM_CHUNKS
+      DMA2D_REPEAT
     );
     snrt_dma_start_2d(
-      b,
+      b0,
       dotp_B_dram + load_idx,
       CHUNK_SIZE * T_S,
       2 * CHUNK_SIZE * T_S,
       CHUNK_SIZE * T_S,
-      NUM_CHUNKS
+      DMA2D_REPEAT
     );
-    calc_timer = benchmark_get_cycle();
+    performance_timer = benchmark_get_cycle();
   }
 
   snrt_cluster_hw_barrier();
 
-  if (cid == 0)
-    start_kernel();
-
-  unsigned iter = 0;
-
   do {
 
     // Wait for the data load of the current calc data
-    if (cid == 0)
+    if (cid == 0) {
+#ifdef TIMING
+      unsigned dma_wait = benchmark_get_cycle();
+#endif
       snrt_dma_wait_all();
+#ifdef TIMING
+      dma_wait = benchmark_get_cycle() - dma_wait;
+      dma_wait_tot += dma_wait;
+#endif
+    }
 
-    load_idx += CHUNK_SIZE * NUM_CHUNKS;
+    load_idx += NUM_ELEM_PER_ITER;
 
     snrt_cluster_hw_barrier();
 
     // Start the DMA transfer on chunk i + 1
     if (cid == 0) {
       if (load_idx < vec_dim) {
-        unsigned store_offset = ((iter + 1) % 2) * CHUNK_SIZE;
-
+        DEBUG(printf("Load index: %u, vec_dim: %u\n", load_idx, vec_dim), DBG_LVL_DBG);
         snrt_dma_start_2d(
-          a + store_offset,
+          a1,
           dotp_A_dram + load_idx,
-          T_S * CHUNK_SIZE,
+          CHUNK_SIZE * T_S,
           2 * CHUNK_SIZE * T_S,
           CHUNK_SIZE * T_S,
-          NUM_CHUNKS
+          DMA2D_REPEAT
         );
         snrt_dma_start_2d(
-          b + store_offset,
+          b1,
           dotp_B_dram + load_idx,
           CHUNK_SIZE * T_S,
           2 * CHUNK_SIZE * T_S,
           CHUNK_SIZE * T_S,
-          NUM_CHUNKS
+          DMA2D_REPEAT
         );
       }
     }
 
-    // Save the result of iteration i
-    // unsigned calc_width = CHUNCK_SIZE >> 1;
-    // unsigned calc_offset = (iter % 2) * CHUNCK_SIZE + calc_width * cid;
-    unsigned calc_width = CHUNK_SIZE;
-    unsigned left_right = (iter % 2) * CHUNK_SIZE;
-    unsigned core_offset = cid * MEMORY_BANKS;
+#ifdef TIMING
+    if (cid == 0) {
+      calc_timer = benchmark_get_cycle();
+    }
+#endif
 
-    unsigned calc_offset = left_right + core_offset;
+    DEBUG(printf("Core %u - Starting fdotp_v64b_ma with a0: %p, b0: %p, CORE_OFFSET: %u, NUM_ELEM_PER_CORE: %u\n",
+           cid, a0 + CORE_OFFSET, b0 + CORE_OFFSET,
+           CORE_OFFSET, NUM_ELEM_PER_CORE), DBG_LVL_DBG);
     result[cid] = fdotp_v64b_ma(
-      a + calc_offset,
-      b + calc_offset,
-      calc_width * NUM_CHUNKS / num_cores,
+      a0 + CORE_OFFSET,
+      b0 + CORE_OFFSET,
+      NUM_ELEM_PER_CORE,
       result[cid]
     );
 
-    iter++;
+#ifdef TIMING
+    if (cid == 0) {
+      calc_timer = benchmark_get_cycle() - calc_timer;
+      calc_timer_avg += calc_timer;
+    }
+#endif
+
+  // Swap the pointers
+    temp = a0;
+    a0 = a1;
+    a1 = temp;
+
+    temp = b0;
+    b0 = b1;
+    b1 = temp;
+
   } while (load_idx < vec_dim);
 
   snrt_cluster_hw_barrier();
 
-  // printf("Result of core %u: %f\n", cid, result[cid]);
-
   if (cid == 0) {
-    calc_timer = benchmark_get_cycle() - calc_timer;
+    performance_timer = benchmark_get_cycle() - performance_timer;
   }
 
-  snrt_cluster_hw_barrier();
+#ifdef TIMING
+  if (cid == 0) {
+    printf("The DMA wait took %u cycles on average.\n", dma_wait_tot / iter);
+    printf("The calculation took %u cycles on average.\n", calc_timer_avg / iter);
+  }
+#endif
 
   // End dump, Record the time
   if (cid == 0) {
-    stop_kernel();
-
-    long unsigned int performance = 1000 * 2 * dotp_l.M / calc_timer;
+    long unsigned int performance = 1000 * 2 * vec_dim / performance_timer;
     long unsigned int utilization =
         performance / (2 * num_cores * SNRT_NFPU_PER_CORE);
 
-    printf("\n----- (%d) dp fdotp MA -----\n", dotp_l.M);
-    printf("The calculation took %u cycles.\n", calc_timer);
+    printf("\n----- (%d) dp fdotp MA -----\n", vec_dim);
+    printf("The calculation took %u cycles.\n", performance_timer);
     printf("The performance is %ld OP/1000cycle (%ld%%o utilization).\n",
            performance, utilization);
-  }
-
-  snrt_cluster_hw_barrier();
-
-  // Accumulate the result into res
-  double res = 0.0;
-  if (cid == 0) {
-    // DEBUG(printf("Accumulate: %f, %f\n", result[0], result[1]), DBG_LVL_ERR);
-    res = vreduce_sum(result, num_cores);
-    DEBUG(printf("Final result: %f\n", res), DBG_LVL_ERR);
   }
 
   snrt_cluster_hw_barrier();
@@ -252,148 +287,160 @@ double dp_dotp_db_ma(
   return res;
 }
 
-/**
- * @brief Implementation of the dot product using simple double buffering without being aware of the L1 memory layout.
- *
- * This function computes the dot product of two vectors A and B of size `dim`
- */
 double dp_dotp_db_simple(
   double *dotp_A_dram,
   double *dotp_B_dram,
   unsigned int vec_dim,
 
   double *l1_buf,
-  unsigned int l1_buf_len, // In sizeof doubles
-
-  double *result
+  unsigned int l1_buf_len // In sizeof doubles
 ) {
   const unsigned int cid = snrt_cluster_core_idx();
   const unsigned int num_cores = snrt_cluster_core_num();
 
-  result[cid] = 0.0;
-
-  const unsigned int MEMORY_BANKS = 16;
   const unsigned int T_S = sizeof(double);
-  const unsigned int CHUNCK_SIZE = MEMORY_BANKS / 2;
 
-  const unsigned int NUM_CHUNCKS = l1_buf_len / 2 / 2 / CHUNCK_SIZE;
-  double *a = l1_buf; // First half for the first vector
-  double *b = l1_buf + (l1_buf_len / 2); // Second half for the second vector
-
-  // Reset timer
-  unsigned int load_timer = (unsigned int)-1;
-  unsigned int calc_timer = (unsigned int)-1;
-
-  unsigned int load_idx = 0;
-
-  unsigned num_iter = vec_dim / (CHUNCK_SIZE * NUM_CHUNCKS);
-  if (cid == 0) {
-    DEBUG(printf("Expected iterations: %u\n", num_iter), DBG_LVL_DBG);
+  if (l1_buf_len % 32 != 0) {
+    printf("L1 buffer length must be a multiple of the number of banks times the number of cores (32).\n");
+    return 0.0;
   }
 
+  // Split the buffer into left and right side.
+  double *a0 = l1_buf;
+  double *b0 = l1_buf + (l1_buf_len / 4);
+  double *a1 = l1_buf + (l1_buf_len / 2);
+  double *b1 = l1_buf + (3 * l1_buf_len / 4);
+  double *temp = NULL;
+  double res = 0.0;
+  result[cid] = 0.0;
+
+  // Pointer increment
+  const unsigned int NUM_ELEM_PER_ITER = b0 - a0 > vec_dim / 2 ? vec_dim / 2 : b0 - a0;
+  const unsigned int NUM_ELEM_PER_CORE = NUM_ELEM_PER_ITER / num_cores;
+  const unsigned int CORE_OFFSET = cid * NUM_ELEM_PER_CORE;
+  printf("NUM_ELEM_PER_ITER: %u\n", NUM_ELEM_PER_ITER);
+
+  // Timing variables
+  unsigned int calc_timer = 0;
+  unsigned int calc_timer_avg = 0;
+  unsigned dma_wait_tot = 0;
+  unsigned int performance_timer = 0;
+
+  // Which elements to load next in doubles
+  unsigned int load_idx = 0;
+
   if (cid == 0) {
     snrt_dma_start_1d(
-      a,
+      a0,
       dotp_A_dram + load_idx, // load index is in size of type
-      CHUNCK_SIZE * NUM_CHUNCKS * T_S // in bytes
+      NUM_ELEM_PER_ITER * T_S // in bytes
     );
     snrt_dma_start_1d(
-      b,
-      dotp_B_dram + load_idx, // load index is in size of type
-      CHUNCK_SIZE * NUM_CHUNCKS * T_S // in bytes
+      b0,
+      dotp_B_dram + load_idx,
+      NUM_ELEM_PER_ITER * T_S
     );
-    DEBUG(printf("Dram %p, A %p, B %p, Num: %u\n", dotp_A_dram + load_idx, a, b, CHUNCK_SIZE * NUM_CHUNCKS), DBG_LVL_DBG);
-    calc_timer = benchmark_get_cycle();
+    performance_timer = benchmark_get_cycle();
   }
 
   snrt_cluster_hw_barrier();
 
-  if (cid == 0)
-    start_kernel();
-
-  unsigned iter = 0;
-
   do {
     // Wait for the data load of the current calc data
-    if (cid == 0)
+    if (cid == 0) {
+#ifdef TIMING
+      unsigned dma_wait = benchmark_get_cycle();
+#endif
       snrt_dma_wait_all();
+#ifdef TIMING
+      dma_wait = benchmark_get_cycle() - dma_wait;
+      dma_wait_tot += dma_wait;
+#endif
+    }
 
-    load_idx += CHUNCK_SIZE * NUM_CHUNCKS; // increment the index of the next load
+    load_idx += NUM_ELEM_PER_ITER; // increment the index of the next load
 
     snrt_cluster_hw_barrier();
 
-    // Start the DMA transfer on chunk i + 1
+    // Load the i + 1 subvector
     if (cid == 0) {
       if (load_idx < vec_dim) {
-        unsigned store_offset = CHUNCK_SIZE * NUM_CHUNCKS * ((iter + 1) % 2);
-
         snrt_dma_start_1d(
-          a + store_offset,
-          dotp_A_dram + load_idx, // load index is in size of type
-          CHUNCK_SIZE * NUM_CHUNCKS * T_S // in bytes
+          a1,
+          dotp_A_dram + load_idx,
+          NUM_ELEM_PER_ITER * T_S
         );
         snrt_dma_start_1d(
-          b + store_offset,
-          dotp_B_dram + load_idx, // load index is in size of type
-          CHUNCK_SIZE * NUM_CHUNCKS * T_S // in bytes
+          b1,
+          dotp_B_dram + load_idx,
+          NUM_ELEM_PER_ITER * T_S
         );
-        DEBUG(printf("Dram %p, A %p, B %p, Num: %u\n", dotp_A_dram + load_idx, a + store_offset, b + store_offset, CHUNCK_SIZE * NUM_CHUNCKS), DBG_LVL_DBG);
       }
     }
 
-    // Save the result of iteration i of each core
-    unsigned calc_size = CHUNCK_SIZE * NUM_CHUNCKS / num_cores;
-    unsigned calc_offset = (iter % 2) * CHUNCK_SIZE * NUM_CHUNCKS + cid * calc_size;
+    // Calculate the i sub-vector dot product
+#ifdef TIMING
+    if (cid == 0) {
+      calc_timer = benchmark_get_cycle();
+    }
+#endif
 
     result[cid] = fdotp_v64b(
-      a + calc_offset,
-      b + calc_offset,
-      calc_size,
+      a0 + CORE_OFFSET,
+      b0 + CORE_OFFSET,
+      NUM_ELEM_PER_CORE,
       result[cid]
     );
 
-    iter++;
+#ifdef TIMING
+    if (cid == 0) {
+      calc_timer = benchmark_get_cycle() - calc_timer;
+      calc_timer_avg += calc_timer;
+    }
+#endif
+
+  // Swap the pointers
+    temp = a0;
+    a0 = a1;
+    a1 = temp;
+
+    temp = b0;
+    b0 = b1;
+    b1 = temp;
   } while (load_idx < vec_dim);
 
   snrt_cluster_hw_barrier();
 
   if (cid == 0) {
-    calc_timer = benchmark_get_cycle() - calc_timer;
+    performance_timer = benchmark_get_cycle() - performance_timer;
   }
 
-  printf("Result of core %u: %f\n", cid, result[cid]);
 
-  snrt_cluster_hw_barrier();
-
-  // End dump, Record the time
+  #ifdef TIMING
   if (cid == 0) {
-    stop_kernel();
+    printf("The DMA wait took %u cycles on average.\n", dma_wait_tot / iter);
+    printf("The calculation took %u cycles on average.\n", calc_timer_avg / iter);
+  }
+  #endif
 
-    long unsigned int performance = 1000 * 2 * vec_dim / calc_timer;
+  if (cid == 0) {
+    long unsigned int performance = 1000 * 2 * vec_dim / performance_timer;
     long unsigned int utilization =
-        performance / (2 * num_cores * SNRT_NFPU_PER_CORE);
+    performance / (2 * num_cores * SNRT_NFPU_PER_CORE);
 
     printf("\n----- (%d) dp fdotp Simple -----\n", vec_dim);
-    printf("The calculation took %u cycles.\n", calc_timer);
+    printf("The calculation took %u cycles.\n", performance_timer);
     printf("The performance is %ld OP/1000cycle (%ld%%o utilization).\n",
-           performance, utilization);
-  }
+      performance, utilization);
+    }
 
-  snrt_cluster_hw_barrier();
+  printf("Result of core %u: %f\n", cid, res);
 
-  double res = 0.0;
-  // Accumulate the result into res
-  if (cid == 0) {
-    DEBUG(printf("Accumulate: %f, %f\n", result[0], result[1]), DBG_LVL_ERR);
-    res = vreduce_sum(result, num_cores);
-    DEBUG(printf("Final result: %f\n", res), DBG_LVL_ERR);
-  }
-
-  // Wait for all cores to finish
   snrt_cluster_hw_barrier();
 
   return res;
 }
+
 
 int main() {
   int ret = 0;
@@ -404,84 +451,59 @@ int main() {
   const unsigned int dim = dotp_l.M;
   const unsigned int T_S = sizeof(double);
 
+  const unsigned int NUM_BANKS = 16;
+
   const unsigned int SCALAR = 8;
-  const unsigned int BUF_SIZE = 2 * 2 * 8 * SCALAR; // in doubles
+  const unsigned int BUF_SIZE = num_cores * NUM_BANKS * SCALAR; // in doubles
 
 
   // Allocate the vector space in L1 memory
   if (cid == 0) {
     l1_buf = (double *)snrt_l1alloc(BUF_SIZE * T_S);
-    result = (double *)snrt_l1alloc(num_cores * T_S);
-    snrt_memset(result, 0, num_cores * T_S * 2);
-
-    DEBUG(printf("Address of l1_buf: %p\n", l1_buf), DBG_LVL_DBG);
-    DEBUG(printf("Address of result: %p\n", result), DBG_LVL_DBG);
+    result_ma = (double *)snrt_l1alloc(num_cores * T_S);
+    result_simple = (double *)snrt_l1alloc(num_cores * T_S);
+    snrt_memset(result_ma, 0, num_cores * T_S);
+    snrt_memset(result_simple, 0, num_cores * T_S);
   }
-
-  for (unsigned i = 0; i < 2; i++) {
 
   snrt_cluster_hw_barrier();
 
-  double res_ma = dp_dotp_db_ma(
+  result_ma[cid] = dp_dotp_db_ma(
     (double *)dotp_A_dram,
     (double *)dotp_B_dram,
     dim,
     l1_buf,
-    BUF_SIZE,
-    result
+    BUF_SIZE
   );
 
   // Check and display results
   if (cid == 0) {
+    double res_ma = result[0] + result[1];
     if (fp_check(res_ma, dotp_result)) {
       printf("\033[31;1mMA Error\033[0m: Result = %f, Golden = %f\n", res_ma, dotp_result);
       ret = -1;
-    } else {
-      printf("\033[32;1mMA Success\033[0m: Result = %f, Golden = %f\n", res_ma, dotp_result);
     }
   }
 
   snrt_cluster_hw_barrier();
 
-  // Reset the memory
-  if (cid == 0) {
-    snrt_memset(l1_buf, 0, BUF_SIZE * T_S);
-    snrt_memset(result, 0, num_cores * T_S * 2);
-  }
+  // result_simple[cid] = dp_dotp_db_simple(
+  //   (double *)dotp_A_dram,
+  //   (double *)dotp_B_dram,
+  //   dim,
+  //   l1_buf,
+  //   BUF_SIZE
+  // );
 
-  if (cid == 0) {
-    l1_buf = (double *)snrt_l1alloc(BUF_SIZE * T_S);
-    result = (double *)snrt_l1alloc(num_cores * T_S);
-    snrt_memset(result, 0, num_cores * T_S * 2);
-
-    DEBUG(printf("Address of l1_buf: %p\n", l1_buf), DBG_LVL_DBG);
-    DEBUG(printf("Address of result: %p\n", result), DBG_LVL_DBG);
-  }
-
-  snrt_cluster_hw_barrier();
-
-  double res_simple = dp_dotp_db_simple(
-    (double *)dotp_A_dram,
-    (double *)dotp_B_dram,
-    dim,
-    l1_buf,
-    BUF_SIZE,
-    result + 2
-  );
-
-  if (cid == 0) {
-    if (fp_check(res_simple, dotp_result)) {
-      printf("\033[31;1mSimple Error\033[0m: Result = %f, Golden = %f\n", res_simple, dotp_result);
-      ret = -1;
-    } else {
-      printf("\033[32;1mSimple Success\033[0m: Result = %f, Golden = %f\n", res_simple, dotp_result);
-    }
-  }
-  snrt_cluster_hw_barrier();
-
-  printf("\n");
-
-}
+  // if (cid == 0) {
+  //   double res_simple = result[0] + result[1];
+  //   if (fp_check(res_simple, dotp_result)) {
+  //     printf("\033[31;1mSimple Error\033[0m: Result = %f, Golden = %f\n", res_simple, dotp_result);
+  //     ret = -1;
+  //   }
+  // }
+  // snrt_cluster_hw_barrier();
 
   return ret;
 }
+
